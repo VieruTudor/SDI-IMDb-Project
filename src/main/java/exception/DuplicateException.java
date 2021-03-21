3fe988b1769337f81a229946f28a0a3e8fbc1dcb
TonceaAlin <58693426+TonceaAlin@users.noreply.github.com>
2021-03-21 12:27:32 +0200

    nevermind

--- src/main/java/exception/DuplicateException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class DuplicateException extends RuntimeException {
-    public DuplicateException(String s) {
-        super(s);
-    }
-}
--- src/main/java/exception/InexistentEntity.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class InexistentEntity extends RuntimeException {
-    public InexistentEntity(String entity_does_not_exist) {
-        super(entity_does_not_exist);
-    }
-}
