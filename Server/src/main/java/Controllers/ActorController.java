6ae5919849d96e129dce8bb09e5fe641abd1027c
VieruTudor <vieru.tudor12@gmail.com>
2021-03-22 19:39:05 +0200

    implemented controller mapper

--- Server/src/main/java/Controllers/ActorController.java
+++ Server/src/main/java/Controllers/ActorController.java
@@ -1,4 +1,4 @@
-package Controllers;
+package controllers;
 import controller.IActorController;
 import repository.*;
 import domain.*;
--- Server/src/main/java/utils/ControllerMapper.java
+++ Server/src/main/java/utils/ControllerMapper.java
@@ -3,8 +3,18 @@ package utils;
 import java.util.HashMap;
 import java.util.Map;
 
+import controllers.*;
+import database.serializers.ActorDBTranslator;
+import database.serializers.DirectorDBTranslator;
+import database.serializers.MovieDBTranslator;
+import database.serializers.PlayInDBTranslator;
+import repository.DBRepository;
+
 public class ControllerMapper {
     public static Map<String, Object> mapper = new HashMap<>(){{
-
+        put("ActorController", new ActorController(new DBRepository<>(new ActorDBTranslator())));
+        put("MovieController", new MovieController(new DBRepository<>(new MovieDBTranslator())));
+        put("DirectorController", new DirectorController(new DBRepository<>(new DirectorDBTranslator())));
+        put("PlaysInController", new PlaysInController(new DBRepository<>(new PlayInDBTranslator())));
     }};
 }
