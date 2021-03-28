8c94c2bda2c83dd68c477fd984564095ff5db82f
TonceaAlin <58693426+TonceaAlin@users.noreply.github.com>
2021-03-28 12:32:30 +0300

    restructured for the next lab

---
+++ socket/Server/src/main/java/server/Server.java
@@ -0,0 +1,37 @@
+package server;
+
+import networking.ServerInformation;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+
+public class Server {
+    private Boolean running = true;
+    private ExecutorService executorService;
+
+
+
+    public Server(ExecutorService srv) {
+        this.executorService = srv;
+    }
+
+    public void run(){
+        try (ServerSocket serverSocket = new ServerSocket(ServerInformation.PORT))
+        {
+            System.out.println("server started ");
+            while(running){
+                Socket client = serverSocket.accept();
+                System.out.println(client.getLocalAddress());
+                executorService.submit(new HandleRequest(client));
+            }
+
+            executorService.shutdown();
+        }
+        catch (IOException e) {
+            // do something
+            e.printStackTrace();
+        }
+    }
+}
