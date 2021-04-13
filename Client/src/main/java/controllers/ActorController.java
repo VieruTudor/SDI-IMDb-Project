4a52f96ca090dcfbf82b7f48760a5e31c5476705
TonceaAlin <58693426+TonceaAlin@users.noreply.github.com>
2021-04-13 17:17:42 +0300

    final cleanup

--- Client/src/main/java/controllers/ActorController.java
+++ Client/src/main/java/controllers/ActorController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class ActorController implements FutureActorController {
 
-
     @Autowired
     private IActorService actorController;
 
